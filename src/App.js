c75bb7653a96819feded7979e2e9a5ab77042f33
Your Name <you@example.com>
2022-08-24 21:12:09 +0900

    Fade Animation With Framer Motion

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-import { v4 as uuidv4} from 'uuid';
-
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -23,7 +22,7 @@ function App() {
 
   function addFeedback(newFeedback) {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback])
+    setFeedback([newFeedback, ...feedback]);
   }
 
   return (
