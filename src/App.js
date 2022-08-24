bd1561e22d46d2fca9b1adf7eca22033cda9d662
Your Name <you@example.com>
2022-08-24 18:01:52 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const title = "Blog Post";
+  const body = "this is my blog post";
+  const comments = [
+    {
+      id: 1,
+      text: "comment one",
+    },
+    {
+      id: 2,
+      text: "comment two",
+    },
+    {
+      id: 3,
+      text: "comment three",
+    },
+  ];
+
+  const loading = false;
+  const showComments = false;
+  const commentBlock = (
+    <div className="comments">
+      <h3> Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => {
+          return <li key={index}>{comment.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
 }
 
-export default App
+export default App;
