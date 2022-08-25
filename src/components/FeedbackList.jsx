549e285b687833f13909b52b8eb34150d91f9836
Your Name <you@example.com>
2022-08-25 22:04:32 +0900

    Spinner Component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
+import Spinner from './shared/Spinner';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback yet</p>;
   }
-  return (
+
+  return isLoading ? (
+   <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => {
@@ -28,6 +32,7 @@ function FeedbackList() {
       </AnimatePresence>
     </div>
   );
+
   //   return (
   //     <div className='feedback-list'>
 
