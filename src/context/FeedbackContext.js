354997ef1604e5f24ef55a4216dc930220fd6729
Your Name <you@example.com>
2022-08-25 21:56:53 +0900

    Fetch Data From JSON-Server Backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,29 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const deleteFeedback = (id) => {
     //  console.log("app", id)
@@ -25,9 +38,11 @@ export const FeedbackProvider = ({ children }) => {
 
   // Feedback更新
   const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item)=>{
-      return item.id === id ? {...item, ...updItem} : item
-    }))
+    setFeedback(
+      feedback.map((item) => {
+        return item.id === id ? { ...item, ...updItem } : item;
+      }),
+    );
   };
 
   const editFeedback = (item) => {
@@ -51,6 +66,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback, // Function
         feedbackEdit, // State
         updateFeedback,
+        isLoading
       }}
     >
       {children}
